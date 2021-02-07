a67fb9cc39e9c11f3cb51b02890d0028408b12c8
mgorzelak <milena.gorzelak5@gmail.com>
2021-02-07 20:17:44 +0100

    v3

---
+++ src/js/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Content from './components/Content';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <Content />
+      </div>
+    )
+  }
+}
+
